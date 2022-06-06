35d4db8967582878135a0f8942bebc37a241d8dd
Michael <michaelgbenle@gmail.com>
2022-06-06 15:50:15 +0100

    Jumia questions solved

---
+++ jumia.go
@@ -0,0 +1,40 @@
+package main
+
+import "sort"
+
+//1
+//select id , name from customer order by name desc, id
+
+//2
+//select max (travel_count.tour) from (select (select count(*)
+//as num from countries c where c.min_size <= f.family_size)
+//as tour from families f) as travel_count
+
+func doubleSizeArr(arr []int64, b int64) int64 {
+	num := b
+	sort.SliceStable(arr, func(a, b int) bool {
+		return arr[a] < arr[b]
+	})
+
+	for _, a := range arr {
+		if a == num {
+			num = num + 2
+		}
+	}
+	return num
+}
+
+// select distinct p.Name, c.Name from schedule s left join Professor p on
+//p.id = s.professor_id left join Course c on c.id = s.course_id inner join
+//Department d on d.id = p.department_id
+
+func closedPaths(number int32) int32 {
+	var sum int32
+	for number > 0 {
+		if number%10 == 0 || number%10 == 4 || number%10 == 6 || number%10 == 9 {
+			sum += 1
+		}
+		number /= 10
+	}
+	return sum
+}
