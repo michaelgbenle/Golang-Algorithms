9a8751ec80f8447094ffeca83d2d4808f916e57f
Michael <michaelgbenle@gmail.com>
2022-06-11 00:22:29 +0100

    cat and mouse

--- catAndMouse.go
+++ catAndMouse.go
@@ -5,5 +5,5 @@ import "math"
 //https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true
 
 func catAndMouse(x int32, y int32, z int32) string {
-	po1 := math.Abs(float64
+	po1 := math.Abs(float64(z - x))
 }
