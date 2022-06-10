f7e75422f1f35097cff43e70d338ddd9d99d5040
Michael <michaelgbenle@gmail.com>
2022-06-11 00:21:44 +0100

    cat and mouse

--- catAndMouse.go
+++ catAndMouse.go
@@ -1,7 +1,9 @@
 package main
 
+import "math"
+
 //https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true
 
 func catAndMouse(x int32, y int32, z int32) string {
-	po1 :=
+	po1 := math.Abs
 }
