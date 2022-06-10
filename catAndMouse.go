c12dc7f3703fde7e7c87454dd3ab4bf49335b048
Michael <michaelgbenle@gmail.com>
2022-06-11 00:25:08 +0100

    cat and mouse

--- catAndMouse.go
+++ catAndMouse.go
@@ -7,5 +7,7 @@ import "math"
 func catAndMouse(x int32, y int32, z int32) string {
 	po1 := math.Abs(float64(z - x))
 	po2 := math.Abs(float64(z - y))
-	if po1 <po2 {
+	if po1 < po2 {
+		return "Cat A"
+	}
 }
