2507d0a3368958f5d3942799e8d3dc163836feb6
michaelgbenle <michaelgbenle@gmail.com>
2022-07-25 16:16:38 -0700

    refactoring

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ func main() {
 	fmt.Println(boomerang([]int{2, 2, 2, 2, 3, 2, 3}))
 	fmt.Println(backToHome("NENESSWW"))
 	fmt.Println(backToHome("EEWE"))
+	fmt.Println(perimeter("s", 7))
+	fmt.Println(perimeter("s", 4))
 
 }
 
--- perimeters.go
+++ perimeters.go
@@ -1,19 +1,12 @@
 package main
 
-import "fmt"
+
 
 //Write a function that takes a number and returns the perimeter
 //of either a circle or a square. The input will be in the form (letter l, number num)
 //where the letter will be either "s" for square, or "c" for circle, and the number will be the
 //side of the square or the radius of the circle.
 
-func main() {
-	fmt.Println(perimeter("s", 7))
-	fmt.Println(perimeter("s", 4))
-	fmt.Println(perimeter("c", 1))
-	fmt.Println(perimeter("c", 4))
-	fmt.Println(perimeter("c", 9))
-}
 
 func perimeter(x string, y float64) float64 {
 	if x == "s" {
