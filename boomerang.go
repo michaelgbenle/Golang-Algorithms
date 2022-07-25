b483a46abae3b6bc49b54483256be34c90abc4b1
michaelgbenle <michaelgbenle@gmail.com>
2022-07-25 16:08:10 -0700

    refactoring

--- boomerang.go
+++ boomerang.go
@@ -6,11 +6,10 @@ func main() {
 	fmt.Println(boomerang([]int{5, 9, 5, 9, 5}))
 	fmt.Println(boomerang([]int{9, 5, 9, 5, 1, 1, 1}))
 	fmt.Println(boomerang([]int{5, 6, 6, 7, 6, 3, 9}))
-	fmt.Println(boomerang([]int{3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2}))
-	fmt.Println(boomerang([]int{2, 2, 2, 2, 3, 2, 3}))
+	
 }
 
-func boomerang(boo []int) int {
+func Boomerang(boo []int) int {
 	var count int
 	for i := 0; i < len(boo)-2; i++ {
 		var k = i + 1
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-func main (){
+import "fmt"
 
-	
+func main() {
+	fmt.Println(Boomerang([]int{3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2}))
+	fmt.Println(boomerang([]int{2, 2, 2, 2, 3, 2, 3}))
+
 }
+
