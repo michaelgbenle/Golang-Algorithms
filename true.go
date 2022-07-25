0e16c7ec105b1dd290967b40d3cc11c8bf56c0a2
michaelgbenle <michaelgbenle@gmail.com>
2022-07-25 16:26:11 -0700

    refactoring

--- true.go
+++ true.go
@@ -1,16 +1,8 @@
 package main
 
-import "fmt"
 
-func main() {
 
-	result := countTrue([]bool{true, false, false, true, false})
-	result2 := countTrue([]bool{false, false, false, false})
-	result3 := countTrue([]bool{false, true, false, false})
-	fmt.Printf("%v\n", result)
-	fmt.Printf("%v\n", result2)
-	fmt.Printf("%v\n", result3)
-}
+
 
 func countTrue(items []bool) int {
 	var count int
