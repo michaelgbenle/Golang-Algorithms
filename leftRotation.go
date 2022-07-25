ac76527170423e7870bab253a68764e4dbe995d7
michaelgbenle <michaelgbenle@gmail.com>
2022-07-25 16:19:12 -0700

    refactoring

--- leftRotation.go
+++ leftRotation.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
+
 
 //https://www.hackerrank.com/contests/week-6-monday/challenges/array-left-rotation
-func main() {
-	
-}
+
 
 func rotateLeft(d int32, arr []int32) []int32 {
 	a := arr[:d]
