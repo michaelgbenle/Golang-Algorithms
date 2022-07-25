bb3bec1229eb5fcae758b54c19eecc0e9c023f8d
michaelgbenle <michaelgbenle@gmail.com>
2022-07-25 16:20:40 -0700

    refactoring

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ func main() {
 	fmt.Println(trackTheRobot([]int{10, -10, -10, 10}))
 	fmt.Println(trackTheRobot([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
 	fmt.Println(trackTheRobot([]int{1, 0, 2, 0, 3, 0, 4, 0, 5, 0}))
+	fmt.Println(upHill(18, 20, 60))
+	fmt.Println(upHill(30, 10, 30))
 
 }
 
--- upTheHill.go
+++ upTheHill.go
@@ -1,18 +1,9 @@
 package main
 
-import "fmt"
 
 //Write a function that returns the average speed traveled given an uphill time,
 //uphill rate and a downhill rate. Uphill time is given in minutes. Return the
 //rate as an integer (mph). No rounding is necessary.
-
-func main() {
-	fmt.Println(upHill(18, 20, 60))
-	fmt.Println(upHill(30, 10, 30))
-	fmt.Println(upHill(18, 20, 60))
-	fmt.Println(upHill(30, 8, 24))
-	fmt.Println(upHill(18, 10, 30))
-}
 
 func upHill(a, b, c float64) float64 {
 	t1 := a / 60
